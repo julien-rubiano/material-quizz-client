a72b4820f69a4e840b45e8f7239f63c686484166
julien-rubiano <32417791+julien-rubiano@users.noreply.github.com>
2022-05-16 13:08:05 +0200

    Add quizz play component

--- frontend/src/app/components/quizz/quizz-play/quizz-play.component.ts
+++ frontend/src/app/components/quizz/quizz-play/quizz-play.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-quizz-play',
   templateUrl: './quizz-play.component.html',
-  styleUrls: ['./quizz-play.component.css']
+  styleUrls: ['./quizz-play.component.css'],
 })
 export class QuizzPlayComponent implements OnInit {
+  isLinear = false;
+  firstFormGroup!: FormGroup;
+  secondFormGroup!: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.firstFormGroup = this.fb.group({
+      firstCtrl: ['', Validators.required],
+    });
+    this.secondFormGroup = this.fb.group({
+      secondCtrl: ['', Validators.required],
+    });
   }
-
 }
